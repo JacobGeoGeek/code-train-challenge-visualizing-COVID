3e5be8f4c60fc77c1ff08268bb618cd71d148b57
JacobGeoGeek <jacobchaar@live.ca>
2020-10-27 11:41:49 -0400

    build the map with covid Data

--- sketch.js
+++ sketch.js
@@ -2,6 +2,8 @@ const mappa = new Mappa("Leaflet");
 
 let covidData;
 let myMap;
+let countries;
+let dataProcess = [];
 
 const options = {
   lat: 0,
@@ -10,25 +12,47 @@ const options = {
   style: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
 };
 
-function getCovidData() {
-  fetch("https://rapidapi.p.rapidapi.com/api", {
-    method: "GET",
-    headers: {
-      "x-rapidapi-host": "corona-virus-world-and-india-data.p.rapidapi.com",
-      "x-rapidapi-key": "9Zn0q7vRfkmshAkYYPYUbwWPNRBep19vtwSjsn8daKMRir7wq4",
-    },
-  })
-    .then((response) => response.json())
-    .then((response) => {
-      covidData = response;
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+function processData() {
+  for (let row of countries.rows) {
+    let countryName = row.get("name");
+    let lat = row.get("latitude");
+    let long = row.get("longitude");
+    let countryStats = covidData.countries_stat.find((x) => x.country_name === countryName);
+
+    if (countryStats) {
+      let numberCases = Number(countryStats.cases.replace(/,/g, ""));
+      let diameter = sqrt(numberCases);
+      dataProcess.push({ countryName, lat, long, numberCases });
+    }
+  }
+  let minCases = dataProcess.reduce((prev, curr) => (prev.numberCases < curr.numberCases ? prev : curr));
+  let maxCases = dataProcess.reduce((prev, curr) => (prev.numberCases > curr.numberCases ? prev : curr));
+
+  let minDiameter = sqrt(minCases.numberCases);
+  let maxDiameter = sqrt(maxCases.numberCases);
+  dataProcess.forEach((country) => {
+    country.diameter = map(sqrt(country.numberCases), minDiameter, maxDiameter, 1, 30);
+  });
 }
 
 function preload() {
-  getCovidData();
+  countries = loadTable("countries.csv", "csv", "header");
+  httpDo(
+    "https://rapidapi.p.rapidapi.com/api",
+    {
+      headers: {
+        "x-rapidapi-host": "corona-virus-world-and-india-data.p.rapidapi.com",
+        "x-rapidapi-key": "9Zn0q7vRfkmshAkYYPYUbwWPNRBep19vtwSjsn8daKMRir7wq4",
+      },
+    },
+    function (res) {
+      covidData = JSON.parse(res);
+      processData();
+    },
+    function (err) {
+      console.log(err);
+    }
+  );
 }
 
 function setup() {
@@ -41,7 +65,12 @@ function setup() {
 
 function draw() {
   clear();
-  const nigeria = myMap.latLngToPixel(11.396396, 5.076543);
-  // Using that position, draw an ellipse
-  ellipse(nigeria.x, nigeria.y, 20, 20);
+  if (dataProcess.length !== 0) {
+    dataProcess.forEach((country) => {
+      const pixel = myMap.latLngToPixel(country.lat, country.long);
+      fill(255, 0, 200, 100);
+      const scale = pow(2, myMap.zoom());
+      ellipse(pixel.x, pixel.y, country.diameter * scale);
+    });
+  }
 }
